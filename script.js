6f9530d886424eff404e9bf12f1a2a1e3eb8cde2
bassfull <isaias.barbosa123@hotmail.comgit config --global user.email isaias.barbosa123@hotmail.comgit config --global user.email isaias.barbosa123@hotmail.com>
2022-07-18 22:49:45 -0300

    Monitor do Input Range

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 
 var menuu = document.querySelector(".menu");
 
-    menuu.addEventListener("click", function() { var menu = document.querySelector(".topbar").classList.toggle("showmenu");
+    menuu.addEventListener("click", function() { 
+        var menu = document.querySelector(".topbar").classList.toggle("showmenu");
 });
+
+
+let element = document.getElementById('textInput')
+element.innerText = document.getElementById('ranger').value + ' Dias'
+
+function updateTextInput(val) {
+   document.getElementById('textInput').innerText=val + ' Dias';
+   
+  }
