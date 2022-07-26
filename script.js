649baf2693d0fdff86c138d49ce3a55acf99ed6c
bassfull <isaias.barbosa123@hotmail.comgit config --global user.email isaias.barbosa123@hotmail.comgit config --global user.email isaias.barbosa123@hotmail.com>
2022-07-25 22:25:30 -0300

    Programando aba orçamento

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-
 var menuu = document.querySelector(".menu");
 
-    menuu.addEventListener("click", function() { 
-        var menu = document.querySelector(".topbar").classList.toggle("showmenu");
+let qtdPaginas = document.querySelector(".qtdPaginas");
+    qtdPaginas.value = 1
+
+menuu.addEventListener("click", function () {
+  var menu = document
+    .querySelector(".topbar")
+    .classList.toggle("showmenu");
 });
-
-
-let element = document.getElementById('textInput')
-element.innerText = document.getElementById('ranger').value + ' Dias'
+let element = document.getElementById("textInput");
+element.innerText =
+  document.getElementById("ranger").value + " Dias";
 
 function updateTextInput(val) {
-   document.getElementById('textInput').innerText=val + ' Dias';
-   
+  document.getElementById("textInput").innerText =
+    val + " Dias";
+}
+
+function orcamento() {
+  let groupLayout = 0;
+  qtdPaginas = document.querySelector(".qtdPaginas").value;
+  textInput = document.querySelector("#textInput").value;
+  if (document.querySelector("#layout-nao").value == "on") {
+    groupLayout = 500;
   }
+  valorDias = qtdPaginas * 100;
+  (valorOrcamento = valorDias++), textInput++, groupLayout;
+  return valorOrcamento;
+}
+
+console.log(document.querySelector("#layout-sim").value);
+console.log(orcamento());
